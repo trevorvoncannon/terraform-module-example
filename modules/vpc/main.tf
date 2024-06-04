2c80e46462b9ef04e074c08b6d93bdd873465295
trevorvoncannon <trevor.voncannon@c2fo.com>
2024-06-04 06:40:03 -0500

    Misread instructions, removing NAT gateway

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -38,19 +38,6 @@ resource "aws_internet_gateway" "igw" {
   #}
 }
 
-resource "aws_eip" "nat_eip" {
-  depends_on = [aws_internet_gateway.igw]
-}
-
-resource "aws_nat_gateway" "nat_gw" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public.id
-  depends_on    = [aws_internet_gateway.igw, aws_subnet.public]
-  #tags = {
-  #  Name        = "${var.prefix}-natgw"
-  #}
-}
-
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.vpc.id
   availability_zone = "us-west-2b"
@@ -61,18 +48,6 @@ resource "aws_subnet" "private2" {
   vpc_id            = aws_vpc.vpc.id
   availability_zone = "us-west-2c"
   cidr_block        = "172.55.3.0/24"
-}
-
-resource "aws_route_table" "nat_gateway" {
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block        = "0.0.0.0/0"
-    nat_gateway_id     = aws_nat_gateway.nat_gw.id
-  }
-  #tags = {
-  #  Name        = "${var.prefix}-private-rt"
-  #}
 }
 
 resource "aws_route_table" "internet_gateway" {
